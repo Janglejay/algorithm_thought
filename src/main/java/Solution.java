26e7b68fd583a0af0e8bcd67d1461b0cd5550646
ForU <18644045901@163.com>
2021-02-10 12:11:57 +0800

    image link fixed

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,26 +1,32 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
-    public int subarraysWithKDistinct(int[] arr, int k) {
-        return getNumber(arr, k) - getNumber(arr, k - 1);
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = solution.minWindow("ADOBECODEBANC", "ABC");
+        System.out.println(s);
     }
-    private int getNumber(int[] arr, int k) {
-        int res = 0;
-        int n = arr.length;
-        Map<Integer, Integer> map = new HashMap<>();
-        int l = 0, r = 0;
-        while (r < n) {
-            map.put(arr[r], map.getOrDefault(arr[r], 0) + 1);
-            while (map.size() > k) {
-                int x = arr[l++];
-                int number = map.get(x);
-                number--;
-                if (number <= 0) map.remove(x);
-                else map.put(x, number);
+
+    public String minWindow(String s, String t) {
+        int need = 0;
+        int[] t1 = new int['z' - 'A' + 1];
+        int[] t2 = new int['z' - 'A' + 1];
+        for (char c : t.toCharArray()) {
+            if (t1[c - 'A'] == 0) need++;
+            t1[c - 'A']++;
+        }
+        String res = "";
+        for (int i = 0, j = 0; j < s.length(); j++) {
+            int idx = s.charAt(j) - 'A';
+            t2[idx]++;
+            if (t2[idx] == t1[idx]) need--;
+            while (need == 0) {
+                if ("".equals(res) || j - i + 1 < res.length()) {
+                    res = s.substring(i, j + 1);
+                }
+                int index = s.charAt(i) - 'A';
+                if (t1[index] == t2[index]) need++;
+                t2[index]--;
+                i++;
             }
-            res += r - l + 1;
-            r++;
         }
         return res;
     }
